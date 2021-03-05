32cf0d77343734c6e2f32a8f13296ba8ece2a8fb
Kathleen Welch <ktywelch@gmail.com>
2021-03-04 20:01:11 -0800

    fixed the server.js

--- server.js
+++ server.js
@@ -25,11 +25,12 @@ const apiRoutes = require('./routes/api_routes');
 
 apiRoutes(app);
 
-
-app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`App running on PORT http://localhost:${PORT}`);
